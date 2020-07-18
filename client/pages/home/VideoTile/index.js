849507bbdc516e2b769dd394d27ed5b46a70e182
AlesyaSk <a.skorohod@ydt-global.com>
2020-07-19 01:55:54 +0300

    Added styles for video

--- client/pages/home/VideoTile/index.js
+++ client/pages/home/VideoTile/index.js
@@ -20,7 +20,7 @@ const VideoTile = ({isVisible}) => {
         <small>12.10.2020</small>
         <span className="UpperCase">City</span>
       </div>
-      <video ref={videoElement} src="https://dl5.webmfiles.org/big-buck-bunny_trailer.webm" poster="https://www.rapidtables.com/web/html/link/flower.jpg" controls />
+      <video ref={videoElement} src="https://dl5.webmfiles.org/big-buck-bunny_trailer.webm" poster="https://i.pinimg.com/564x/cd/65/02/cd6502e619912e69a40d032478fd0b44.jpg" controls />
         <div className="SelectedCandidate UpperCase"><span>ЗА ТИХАНОВСКУЮ</span> <img src={tick} className="Tick" alt="tick"/></div>
     </div>
   );
