9651262e10277732040cfc95f50453eb9aa14cdd
AlesyaSk <a.skorohod@ydt-global.com>
2020-07-18 20:21:03 +0300

    Added styles for home page

--- client/pages/home/VideoTile/index.js
+++ client/pages/home/VideoTile/index.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import './styles.scss';
+import tick from '../../../assets/images/tick.png';
 
 const VideoTile = ({isVisible}) => {
   const videoElement = useRef(null);
@@ -10,17 +11,17 @@ const VideoTile = ({isVisible}) => {
     } else {
       videoElement.current.pause();
     }
-  }, [isVisible])
+  }, [isVisible]);
 
   return (
     <div className="VideoElement">
       <div className="Details">
         <span className="UpperCase">Anatoly Semenyaka</span>
-        <span>12.10.2020</span>
+        <small>12.10.2020</small>
         <span className="UpperCase">City</span>
       </div>
       <video ref={videoElement} src="https://dl5.webmfiles.org/big-buck-bunny_trailer.webm" poster="https://www.rapidtables.com/web/html/link/flower.jpg" controls />
-      <strong className="SelectedCandidate UpperCase">ЗА ТИХАНОВСКУЮ</strong>
+        <div className="SelectedCandidate UpperCase"><span>ЗА ТИХАНОВСКУЮ</span> <img src={tick} className="Tick" alt="tick"/></div>
     </div>
   );
 };
